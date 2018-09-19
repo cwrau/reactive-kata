cfffdfeca07d45e2d3d8752bf31a1946732cfe54
Dennis Dierkes <dierkes@smartsquare.de>
2018-09-19 16:39:24 +0200

    Add flux, mono and error handling examples

---
+++ src/test/kotlin/de/smartsquare/dojo/reactive/live/ErrorHandlingOperatorsExample.kt
@@ -0,0 +1,41 @@
+package de.smartsquare.dojo.reactive.live
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
+import reactor.retry.Retry.anyOf
+import java.time.Duration
+
+class ErrorHandlingOperatorsExample {
+
+    @Test
+    fun retry() {
+        Mono.fromCallable { throw RuntimeException("Boom!").also { println("Callable") } }
+                .retry(3)
+                .log()
+                .subscribe()
+    }
+
+    @Test
+    fun timeout() {
+        Mono.fromCallable { Thread.sleep(5000).let { "Data" } }
+                .timeout(Duration.ofMillis(100), Mono.just("Cached Data"))
+                .log()
+                .subscribe(::println)
+    }
+
+    @Test
+    fun `retry when specific exception with exponential backoff`() {
+        Mono.fromCallable { throw IllegalArgumentException().also { println("Callable") } }
+                .subscribeOn(Schedulers.elastic())
+                .retryWhen(
+                        anyOf<IllegalArgumentException>(IllegalArgumentException::class.java)
+                                .exponentialBackoff(Duration.ofMillis(100), Duration.ofSeconds(10))
+                                .retryMax(10)
+                )
+                .log()
+                .subscribe()
+
+        Thread.sleep(10000)
+    }
+}
---
+++ src/test/kotlin/de/smartsquare/dojo/reactive/live/FluxAndMonoExample.kt
@@ -0,0 +1,67 @@
+package de.smartsquare.dojo.reactive.live
+
+import org.junit.jupiter.api.Test
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
+import reactor.core.publisher.toFlux
+import reactor.core.publisher.toMono
+import reactor.core.scheduler.Schedulers
+import java.time.Duration
+import java.util.Arrays.asList
+
+class FluxAndMonoExample {
+
+    @Test
+    fun `creation of a flux`() {
+        Flux.just("another", "coding", "dojo").subscribe(::print)
+
+        // kotlin support 😎
+        asList("another", "coding", "dojo").toFlux().subscribe(::print)
+    }
+
+    @Test
+    fun `creation of a mono`() {
+        Mono.just("astring").subscribe(::print)
+
+        // kotlin support 😎
+        "astring".toMono().subscribe(::print)
+    }
+
+    @Test
+    fun `flux to mono`() {
+        val last: Mono<String> = Flux.just("another", "coding", "dojo")
+                .takeLast(1)
+                .toMono()
+    }
+
+    @Test
+    fun `mono and mono to flux`() {
+        val flux: Flux<String> = "x".toMono().concatWith { "d".toMono() }
+    }
+
+    @Test
+    fun `infinite flux`() {
+        val flux = Flux.interval(Duration.ofMillis(10))
+                .subscribeOn(Schedulers.elastic())
+                .map { "B" }
+                .subscribe(::println)
+
+        Thread.sleep(10000)
+    }
+
+    @Test
+    fun `mono from callable`() {
+        Mono.fromCallable { BlockingApi().fetch() }
+                .subscribeOn(Schedulers.elastic())
+                .subscribe(::println)
+
+        println("Falling asleep for 6 seconds")
+        Thread.sleep(6000)
+        println("Awakening")
+    }
+
+    private class BlockingApi {
+
+        fun fetch() = Thread.sleep(5000).let { "Blocking API Response" }
+    }
+}
