11f1b9bd046c06304d434dc7e85829c47d4d9371
Dennis Dierkes <dierkes@smartsquare.de>
2018-10-24 16:10:12 +0200

    Add main method adapter testing

--- src/main/kotlin/de/smartsquare/dojo/reactive/summarizer/Adapter.kt
+++ src/main/kotlin/de/smartsquare/dojo/reactive/summarizer/Adapter.kt
@@ -1,6 +1,19 @@
 package de.smartsquare.dojo.reactive.summarizer
 
 import de.smartsquare.dojo.reactive.dashboard.Dashboard
+import de.smartsquare.dojo.reactive.dashboard.FragileConsoleDashboard
+import de.smartsquare.dojo.reactive.summarizer.Adapter.Companion.connect
+import de.smartsquare.dojo.reactive.tournament.RandomTournamentGenerator
+import java.lang.Thread.sleep
+
+fun main(args: Array<String>) {
+    val tournament = RandomTournamentGenerator()
+    val summarizer = StatisticsSummarizer(tournament)
+    val dashboard = FragileConsoleDashboard()
+
+    connect(source = summarizer, sink = dashboard)
+    sleep(10000)
+}
 
 class Adapter private constructor() {
 
